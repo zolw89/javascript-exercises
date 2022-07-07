9240ca50062bfe203f13e8aeb5f9b59e551f4354
Rafał Mosakowski <mosakowski.r@gmail.com>
2022-07-07 07:57:15 +0200

    some js exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
+    num = Number(num);
+    
+    let arr = [1, 1];
+    for (let i = 1; i <= num - 2; i++) {
+        arr.push(arr[i-1] + arr[i])
+    }
+    return arr[arr.length-1]
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+const getTheTitles = function(e) {
+return e.map(book => book.title)
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
